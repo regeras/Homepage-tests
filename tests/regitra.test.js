abe69e85224ab0f2d77026c48eeeacfb534deac0
regeras <ambrazaitisrokas@gmail.com>
2025-02-14 19:20:50 +0200

    upd

--- tests/regitra.test.js
+++ tests/regitra.test.js
@@ -15,14 +15,14 @@ test('Services open successfully', async ({ page }) => {
     let homePage = new Homepage(page);
     await homePage.goto();
     await homePage.openServices();
-
+// check to see if 'E.Paslaugos' opens and check for correct url
  })
 
  test('News open successfully', async ({ page }) => {
     let homePage = new Homepage(page);
     await homePage.goto();
     await homePage.openNews();
-
+// check if the 'naujienos' page opens and check for correct url.
 })
 
 
